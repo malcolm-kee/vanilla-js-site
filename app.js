e518efad15c04ed6edf354dd122e2bb361fae2ca
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2021-07-04 13:59:14 +0800

    small tweaks

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
-var express = require('express');
-var path = require('path');
+const express = require('express');
+const path = require('path');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const compression = require('compression');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 const apiRouter = require('./routes/api');
 
-var app = express();
+const app = express();
 
 app.use(logger('dev'));
 app.use(cors());
@@ -23,6 +24,7 @@ app.use(
 
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(compression())
 app.use(
   express.static(path.join(__dirname, 'public'), { extensions: ['html'] })
 );
@@ -31,6 +33,6 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/api', apiRouter);
 
-var listener = app.listen(8080, function () {
+const listener = app.listen(8080, function () {
   console.log('Listening on port ' + listener.address().port);
 });
